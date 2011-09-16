62d3958f08ee54c6352ea3654c6c6122a3f4e8f0
Christian Voigt <github@verpeil.de>
2011-09-16 05:47:20 +0000

    initial project commit

---
+++ src/de/verpeil/Configuration.java
@@ -0,0 +1,6 @@
+package de.verpeil;
+
+public class Configuration {
+	static String URL = "http://geekandpoke.typepad.com/geekandpoke/atom.xml";
+
+}
---
+++ src/de/verpeil/DataProvider.java
@@ -0,0 +1,60 @@
+package de.verpeil;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.Namespace;
+import org.jdom.input.SAXBuilder;
+
+public class DataProvider {
+	private URL url;
+	private Document doc;
+
+
+	public DataProvider() {
+		setURL();
+		configureJDOM();
+	}
+
+	private void setURL() {
+		try {
+			url = new URL(Configuration.URL);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void configureJDOM() {
+		try {
+			doc = new SAXBuilder().build(url);
+		} catch (JDOMException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public URL getLastPicture() {
+		Element atom = doc.getRootElement();
+		Namespace ns = atom.getNamespace();
+		try {
+			String content = atom.getChild("entry", ns).getChild("content", ns)
+					.getValue();
+			String croppedContent = content.split("href=")[1].replace("\"","");
+			url = new URL(croppedContent);
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return url;
+
+	}
+}
