9f238eda65a49b9277a61d0742bedcd1a531e6e0
Christian Voigt <github@verpeil.de>
2011-09-22 19:17:29 +0000

    should be platform independent; ticket #7

--- src/de/verpeil/Commands.java
+++ src/de/verpeil/Commands.java
@@ -3,10 +3,29 @@ package de.verpeil;
 public class Commands {
 
 	public static String DLCommand(String url) {
-		return "./tools/wget/wget -q -O last.jpg -U Mozilla/5.0 " + url;
+		return compileIndependentCommand("wget" ," -q -O last.jpg -U Mozilla/5.0 " + url);
 	}
 
 	public static String convertCommand() {
-		return "./tools/imagick/convert last.jpg last.pdf";
+		return compileIndependentCommand("convert"," last.jpg last.pdf");
+	}
+	
+	private static String compileIndependentCommand(String command, String parameters) {
+		StringBuffer compiledCommand = new StringBuffer("");
+		addWindowsFolderPrefixIfNeeded(compiledCommand, command);
+		compiledCommand.append(command);		
+		compiledCommand.append(parameters);
+		return compiledCommand.toString();
+	}
+	
+	private static void addWindowsFolderPrefixIfNeeded(StringBuffer compiledCommand, String command) {
+		if (isWindows()) {
+			compiledCommand.append("./tools/"+command+"/");
+		}
+		
+	}
+
+	private static boolean isWindows() {
+		return System.getProperty("os.name").contains("Windows");
 	}
 }
