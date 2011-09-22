c4229d9108f212218c280b1fd030a015bf054771
ctvoigt <ctvoigt@7d038fb3-03df-432c-a8bb-5903fe2c91d6>
2011-09-22 19:25:15 +0000

    refactoring

--- trunk/luckyGeek/src/de/verpeil/Commands.java
+++ trunk/luckyGeek/src/de/verpeil/Commands.java
@@ -2,27 +2,43 @@ package de.verpeil;
 
 public class Commands {
 
+	private static final String SPACE = " ";
+
 	public static String DLCommand(String url) {
-		return compileIndependentCommand("wget" ," -q -O last.jpg -U Mozilla/5.0 " + url);
+		return compileIndependentCommand("wget", appendParameter("-q")
+				+ appendParameter("-O last.jpg")
+				+ appendParameter("-U Mozilla/5.0") + appendParameter(url));
 	}
 
 	public static String convertCommand() {
-		return compileIndependentCommand("convert"," last.jpg last.pdf");
+		return compileIndependentCommand("convert", appendParameter("last.jpg")
+				+ appendParameter("last.pdf"));
 	}
-	
-	private static String compileIndependentCommand(String command, String parameters) {
+
+	private static String appendSpace(String string) {
+		return string.concat(SPACE);
+	}
+
+	private static String appendParameter(String string) {
+		return appendSpace(string);
+	}
+
+	private static String compileIndependentCommand(String command,
+			String parameters) {
 		StringBuffer compiledCommand = new StringBuffer("");
 		addWindowsFolderPrefixIfNeeded(compiledCommand, command);
-		compiledCommand.append(command);		
+		command = appendSpace(command);
+		compiledCommand.append(command);
 		compiledCommand.append(parameters);
 		return compiledCommand.toString();
 	}
-	
-	private static void addWindowsFolderPrefixIfNeeded(StringBuffer compiledCommand, String command) {
+
+	private static void addWindowsFolderPrefixIfNeeded(
+			StringBuffer compiledCommand, String command) {
 		if (isWindows()) {
-			compiledCommand.append("./tools/"+command+"/");
+			compiledCommand.append("./tools/" + command + "/");
 		}
-		
+
 	}
 
 	private static boolean isWindows() {
