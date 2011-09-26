70acc062cba92e5daab5af4c4713baa0543dd455
Christian Voigt <github@verpeil.de>
2011-09-26 17:26:02 +0000

    refactoring

--- src/de/verpeil/Main.java
+++ src/de/verpeil/Main.java
@@ -7,32 +7,32 @@ import java.util.logging.Logger;
 import org.apache.pdfbox.util.PDFMergerUtility;
 
 public class Main {
-	private static final Logger LOG = Logger.getLogger(Main.class.getCanonicalName());
+	private static final Logger LOG = Logger.getLogger(Main.class
+			.getCanonicalName());
 	private final FileDownloader fd = new FileDownloader();
 	private final DataProvider dp = new DataProvider();
 
-
 	private void procss() {
 		storeToFile();
-		
+		appendToPDF();
+		cleanUp();
 	}
-	
+
 	void storeToFile() {
 		LOG.info("Begin: " + new Date());
-		File xmlFeed = fd.download(Configuration.getDownloadUrl(), Configuration.getTempXmlName());
+		File xmlFeed = fd.download(Configuration.getDownloadUrl(),
+				Configuration.getTempXmlName());
 		String imageUrl = dp.extractImageUrl(xmlFeed);
-		
+
 		LOG.fine(imageUrl);
 		File image = fd.download(imageUrl, Configuration.getLastImage());
 		LOG.fine(image.getAbsolutePath());
-		
-		appendToPDF(Configuration.getLastFile(), image);
-		
+
 		xmlFeed.deleteOnExit();
 		LOG.info("End: " + new Date());
 	}
 
-	private void appendToPDF(String input, File image) {
+	private void appendToPDF() {
 		LOG.info("Begin append to pdf.");
 		String allPdf = Configuration.getAllFile();
 		String lastPdf = Configuration.getLastFile();
@@ -41,7 +41,7 @@ public class Main {
 		} catch (Exception e) {
 			LOG.severe("Can not transform image to pdf: " + e.getMessage());
 		}
-		
+
 		PDFMergerUtility mergePdf = new PDFMergerUtility();
 		mergePdf.addSource(allPdf);
 		mergePdf.addSource(lastPdf);
@@ -52,8 +52,13 @@ public class Main {
 			LOG.severe("Can not merge pdfs: " + e.getMessage());
 		}
 		LOG.info("End append to pdf.");
-    }
-	
+	}
+
+	private void cleanUp() {
+		// TODO Delte TMP-Files
+
+	}
+
 	public static void main(String[] args) {
 		new Main().procss();
 	}
