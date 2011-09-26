14e32329cbc10148ceabb71bbd77d3b1cedc4b25
ctvoigt <ctvoigt@7d038fb3-03df-432c-a8bb-5903fe2c91d6>
2011-09-26 17:09:26 +0000

    cleanup

--- trunk/luckyGeek/src/de/verpeil/CommandsHelper.java
+++ trunk/luckyGeek/src/de/verpeil/CommandsHelper.java
@@ -5,21 +5,11 @@ public class CommandsHelper {
 
 	private static final String SPACE = " ";
 	private static final String WINDOWS_TOOLS_FOLDER = "./tools/";
-	private static final String DL_COMMAND = "wget";
-	private static final String QUIET_MODE = "-q";
 	private static final String LAST_DL_JPEG = "current.jpg";
 	private static final String LAST_DL_PDF = "last.pdf";
-	private static final String SPEZIFY_OUTPUT_FILENAME = "-O ";
-	private static final String SPEZIFY_USER_AGENT = "-U ";
-	private static final String USER_AGENT = "Mozilla/5.0";
 	private static final String CONVERT_COMMAND = "convert";
 	private static final CharSequence WINDOWS = "Windows"; //TODO Check; better "Win"?
 
-	public static String DLCommand(String url) {
-		return compileIndependentCommand(DL_COMMAND, appendParameter(QUIET_MODE)
-				+ appendParameter(SPEZIFY_OUTPUT_FILENAME+ LAST_DL_JPEG)
-				+ appendParameter(SPEZIFY_USER_AGENT+USER_AGENT) + appendParameter(url));
-	}
 
 	public static String convertCommand() {
 		return compileIndependentCommand(CONVERT_COMMAND, appendParameter(LAST_DL_JPEG)
