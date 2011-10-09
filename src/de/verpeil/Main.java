18e9b2d729b8382cf626d753a5551d7eb2574435
chripo2701 <chripo2701@7d038fb3-03df-432c-a8bb-5903fe2c91d6>
2011-10-09 10:57:14 +0000

    check for all.pdf added

--- src/de/verpeil/Main.java
+++ src/de/verpeil/Main.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.Date;
 import java.util.logging.Logger;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.pdfbox.util.PDFMergerUtility;
 
 public class Main {
@@ -12,6 +13,7 @@ public class Main {
 	private final FileDownloader fd = new FileDownloader();
 	private final DataProvider dp = new DataProvider();
 	private final Memory memory = new Memory();
+	private boolean merged = false;
 	private String imageUrl = "";
 
 	private void process() {
@@ -29,10 +31,10 @@ public class Main {
 	
 	void extractImageUrl() {
 		LOG.info("Begin: extract image url.");
-		File xmlFeed = fd.download(Configuration.getDownloadUrl(),
+		File xml = fd.download(Configuration.getDownloadUrl(),
 				Configuration.getTempXmlName());
-		imageUrl = dp.extractImageUrl(xmlFeed);
-		xmlFeed.deleteOnExit();
+		imageUrl = dp.extractImageUrl(xml);
+		FileUtils.deleteQuietly(xml);
 		LOG.info("End: extract image url: " + imageUrl);
 	}
 	
@@ -41,9 +43,9 @@ public class Main {
 	}
 
 	void storeToFile() {
-		LOG.info("Begin storeToFile.");
+		LOG.info("Begin download.");
 		fd.download(imageUrl, Configuration.getLastImage());
-		LOG.info("End storeToFile.");
+		LOG.info("End download.");
 	}
 
 	private void appendToPDF() {
@@ -51,27 +53,45 @@ public class Main {
 		String allPdf = Configuration.getAllFile();
 		String lastPdf = Configuration.getLastFile();
 		(new ImageToPDFConverter()).convert();
+		
+		if (!exists(lastPdf)){
+			LOG.warning("No pdf for merging found. Cancel merging.");
+			return;
+		}
+		
 		PDFMergerUtility mergePdf = new PDFMergerUtility();
 		mergePdf.addSource(allPdf);
 		mergePdf.addSource(lastPdf);
 		mergePdf.setDestinationFileName(allPdf);
 		try {
 			mergePdf.mergeDocuments();
+			merged = true;
 		} catch (Exception e) {
 			LOG.severe("Can not merge pdfs: " + e.getMessage());
 		}
 		LOG.info("End append to pdf.");
 	}
 	
+	private boolean exists(String path) {
+		File file = new File(path);
+		return file != null && file.exists() && file.isFile();
+	}
+	
 	private void save() {
-		memory.setUrl(imageUrl);
-		memory.save();
+		if (merged) {
+			LOG.info("Saving.");
+			memory.setUrl(imageUrl);
+			memory.save();
+			LOG.info("Saved.");
+		}
 	}
 
 	private void cleanUp() {
-		// TODO Delte TMP-Files
+		LOG.info("Cleaning up.");
+		FileUtils.deleteQuietly(new File(Configuration.getLastFile()));
+		LOG.info("Cleaned up.");
 	}
-
+	
 	public static void main(String[] args) {
 		new Main().process();
 	}
