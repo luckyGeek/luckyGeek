fe06259817a2c71facabe66edd9e5dfe049e1882
ctvoigt <ctvoigt@7d038fb3-03df-432c-a8bb-5903fe2c91d6>
2013-04-06 01:02:24 +0000

    revert

---
+++ trunk/luckyGeek/test/de/verpeil/FileDownloaderTest.java
@@ -0,0 +1,64 @@
+/**********************************
+ * FileDownloaderTest.java
+ * Part of the project "luckyGeek" from
+ * ctvoigt (Christian Voigt), chripo2701  2011.
+ *
+ * 
+ * Email: luckygeek@verpeil.de
+ * 
+ *
+ * 
+ **********************************
+ * 
+ * Test-case for FileDownloader.java.
+ **********************************
+ * 
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ * This program is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+package de.verpeil;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.AfterClass;
+import org.junit.Test;
+
+/**
+ * Tests <code>{@link FileDownloader}</code>.
+ */
+public class FileDownloaderTest {
+	
+	@AfterClass
+	public static void tearDown() {
+		FileUtils.deleteQuietly(UnittestUtil.getFileForUserHome("atom.xml"));
+	}
+
+	@Test
+	public void testDownloadFile() {
+		final String url = "http://geekandpoke.typepad.com/geekandpoke/atom.xml";
+		final File result = new FileDownloader().download(url, UnittestUtil.getFileForUserHome("atom.xml").getAbsolutePath());
+		assertNotNull(result);
+		assertTrue(result.exists());
+		assertTrue(result.isFile());
+		assertFalse(result.isHidden());
+		assertTrue(result.getName().endsWith(".xml"));
+		assertEquals("atom.xml", result.getName());
+	}
+}
