9de5247608242584affebd618a243c2f69da2322
chripo2701 <chripo2701@7d038fb3-03df-432c-a8bb-5903fe2c91d6>
2013-08-16 17:43:17 +0000

    delete run.*-files: will be generated / updated through maven

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ $1 == "-h" -o $1 == "--help" ]
-then
-	echo "Usage: Just execute ./run.sh witout parameters. Configuration is done by .properties files in conf."
-else
-	java -Djava.util.logging.config.file=logging.properties -jar luckyGeek.jar    
-fi
-
